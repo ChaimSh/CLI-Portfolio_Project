24c2b14031b2e4e8e259e3ea8ae13723a9ccfc6c
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-06-17 00:55:39 +0000

    first commit

---
+++ lib/CLI_Portfolio_Project.rb
@@ -0,0 +1,6 @@
+require "CLI_Portfolio_Project/version"
+
+module CLIPortfolioProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/CLI_Portfolio_Project/version.rb
@@ -0,0 +1,3 @@
+module CLIPortfolioProject
+  VERSION = "0.1.0"
+end
