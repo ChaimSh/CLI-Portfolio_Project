f1679ecd057b7574486ec8862f210a05f6d65623
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-06-20 01:11:47 +0000

    continued skelton

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,20 +5,38 @@ class CLIPortfolioProject::CLI
       puts "Please type one of the following: Hayom yom, Rambam"
       input = gets.strip
        if input == "Hayom yom"
-         Hayom_yom
+         hayom_yom
        elsif input == "Rambam"
-         Rambam
+         rambam
        else
          puts "Sorry no such option yet."
        end
     end
   
     
-    def Hayom_yom
+    def hayom_yom
       
       puts "The early sages, who were like angels (may their merit protect us) have already determined that the healing of the soul is like the healing of the body:"
-       end
+    end
   
-  
-  
+    
+    def rambam
+puts <<-DOC
+"Negative Commandment 219 (Digest)
+Muzzling a Working Animal
+
+'You shall not muzzle the ox when it is threshing'—Deuteronomy 25:4.
+
+It is forbidden to prevent an animal from eating from produce upon which it is working—e.g., if it is threshing grain or carrying a bundle of hay. It is even forbidden to prevent it by verbally [shouting at it when it wishes to eat].
+
+Positive Commandment 244 (Digest)
+The Borrower
+
+'And if a man borrows from his neighbor...'—Exodus 22:13.
+
+We are commanded [to follow all the laws outlined in the Torah] regarding one who borrows an object from his fellow."
+   DOC
+   end
+   
+   
 end
