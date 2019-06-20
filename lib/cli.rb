3f344ba329b519eab3ff1a602708192aa69df7f0
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-06-20 00:01:30 +0000

    starting skeleton app

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,8 +1,24 @@
 class CLIPortfolioProject::CLI
   
-   def call
-     puts "hello"
-   end
+    def call
+      puts "Hi! What would you like to learn today?"
+      puts "Please type one of the following: Hayom yom, Rambam"
+      input = gets.strip
+       if input == "Hayom yom"
+         Hayom_yom
+       elsif input == "Rambam"
+         Rambam
+       else
+         puts "Sorry no such option yet."
+       end
+    end
+  
+    
+    def Hayom_yom
+      
+      puts "The early sages, who were like angels (may their merit protect us) have already determined that the healing of the soul is like the healing of the body:"
+       end
+  
   
   
 end
