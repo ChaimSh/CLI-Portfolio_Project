7a89523e034e0c8cf1db42033d89ce8e1a1cee76
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-06-21 01:48:03 +0000

    changes to cli

--- lib/CLI_Portfolio_Project.rb
+++ lib/CLI_Portfolio_Project.rb
@@ -4,5 +4,6 @@ require 'pry'
 require_relative "./CLI_Portfolio_Project/version"
 require_relative "./study"
 require_relative "./cli"
+require_relative "./scrape"
 
 #make config/environment
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,18 +2,23 @@ class CLIPortfolioProject::CLI
   
     def call
       puts "Hi! What would you like to learn today?"
-      puts "Please type one of the following: Hayom yom, Rambam"
-      input = gets.strip
-       if input == "Hayom yom"
+      puts "Please type one of the following: Hayom yom, Rambam or type 'exit'"
+      input = nil
+       while input != "exit"
+       input = gets.strip
+       case input
+       when "Hayom yom"
           CLIPortfolioProject::Daily_Studies.hayom_yom
-       elsif input == "Rambam"
+          puts "To continue learning please type in one of the above options. Otherwise, please type 'exit'. Thank you!"
+       when "Rambam"
          CLIPortfolioProject::Daily_Studies.rambam
+       when "exit"
+         puts "Goodbye, please come back tomorow to learn more!"
        else
          puts "Sorry no such option yet."
        end
-    end
-  
-    
+      end
+     end    
    # def hayom_yom
      # @hayom_yom = CLIPortfolioProject::Daily_Studies.hayom_yom
     #end
