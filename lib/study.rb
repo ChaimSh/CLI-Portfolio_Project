86ba85fe0a1999982a7012b7f466f876f1cbde4f
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-06-21 01:27:39 +0000

    init method

--- lib/study.rb
+++ lib/study.rb
@@ -1,8 +1,13 @@
 class CLIPortfolioProject::Daily_Studies
   
-  attr_accessor :name, :credits, :buy_book_url
+  attr_accessor :name, :credits, :book_url, :book_desc
   
-  
+  def initialize(name, credits, book_uri, book_desc )
+  @name = name
+  @credits = credits
+  @book_uri = book_uri
+  @book_desc = book_desc
+  end
   
   def self.hayom_yom
     puts "hi"
@@ -17,6 +22,6 @@ class CLIPortfolioProject::Daily_Studies
 end
 
 #initialize
-#l/oop
+#loop
 #@@all
 
