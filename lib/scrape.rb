6e96f53ec6425092704a1c308ef987a26c82f152
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-06-23 19:47:47 +0000

    hayom yom scrape

--- lib/scrape.rb
+++ lib/scrape.rb
@@ -29,11 +29,11 @@ class CLIPortfolioProject::Daily_Studies_Scrape
   
   
   def self.hayom_yom_scrape
-    doc = Nokogiri::HTML(open("https://www.chabad.org/dailystudy/seferHamitzvos.asp?"))
+    doc = Nokogiri::HTML(open("https://www.chabad.org/dailystudy/hayomyom.asp?"))
     
     hayom_yom = self.new
-    hayom_yom.name = doc.search("tr.hayom-yom-date").text
-    hayom_yom.text = doc.css("div.co_body p").text
+    hayom_yom.name = doc.search("tr.hayom-yom-date").first.text
+    hayom_yom.text = doc.css("div.co_body p").first.text
     hayom_yom.credits = doc.search("div.credit_text_item").text
     hayom_yom
   end
--- lib/study.rb
+++ lib/study.rb
@@ -19,7 +19,7 @@ class CLIPortfolioProject::Daily_Studies
     #job of this method is to scrape the above attributes of hayom yom(perhaps giving option if to see credits or not etc.)
     #to-do:build out hayom_yom with all its attributes
     #the below is to simply scrape the text of the hayom yom in another place
-    CLIPortfolioProject::Daily_Studies_Scrape.hayom_yom
+    CLIPortfolioProject::Daily_Studies_Scrape.hayom_yom_scrape
   end
   
  def self.rambam
