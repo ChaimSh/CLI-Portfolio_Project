ae58615d0d9b5e9955188af2ba24cbd9f3ffeb49
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-06-23 19:35:23 +0000

    hayom yom scrape

--- lib/scrape.rb
+++ lib/scrape.rb
@@ -9,7 +9,7 @@ class CLIPortfolioProject::Daily_Studies_Scrape
   def self.study_array
   study_array = []
   study_array << rambam_scrape
-  study_array << hayom_yom
+  study_array << hayom_yom_scrape
   study_array
   end
   
@@ -26,6 +26,17 @@ class CLIPortfolioProject::Daily_Studies_Scrape
     rambam
   end
   
+  
+  
+  def self.hayom_yom_scrape
+    doc = Nokogiri::HTML(open("https://www.chabad.org/dailystudy/seferHamitzvos.asp?"))
+    
+    hayom_yom = self.new
+    hayom_yom.name = doc.search("tr.hayom-yom-date").text
+    hayom_yom.text = doc.css("div.co_body p").text
+    hayom_yom.credits = doc.search("div.credit_text_item").text
+    hayom_yom
+  end
   
   
 
