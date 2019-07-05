d9200fee2f816cf5e9a0d370ef9d35e1f9ac9f48
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-05 00:50:47 +0000

    refractor

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,22 +1,25 @@
 class CLIPortfolioProject::CLI
   
+  attr_accessor :input, :find_by_text_study
   
     def call
      
       
       puts "Hi! What would you like to learn today?"
       puts "Please type one of the following: Hayom yom, Rambam or type 'exit'"
-      shiurim = CLIPortfolioProject::Daily_Studies.new
-      shiurim.scrape_all
-        input = nil
-        while input != "exit"
-        input = gets.strip
+       #shiurim = CLIPortfolioProject::Daily_Studies.new
+      # shiurim.scrape_all
+        @input = nil
+        while @input != "exit"
+        @input = gets.strip
         case input
           when "Hayom yom"
-            puts shiurim.hayom_yom.text
+           CLIPortfolioProject::CLI.hayom_yom
+           #puts shiurim.find_by_text_study(input)
             puts "To continue learning please type in one of the above options. Otherwise, please type 'exit'. Thank you!"
           when "Rambam"
-           
+            #shiurim.find_by_text_study(input)
+            CLIPortfolioProject::CLI.rambam
             puts "To continue learning please type in one of the above options. Otherwise, please type 'exit'. Thank you!"
           when "exit"
             puts "Goodbye, please come back tomorrow to learn more!"
@@ -27,43 +30,74 @@ class CLIPortfolioProject::CLI
     end    
   
     
+  # def self.hayom_yom
+  #   puts "Hayom Yom:"
+  #   hayom_yom = CLIPortfolioProject::Daily_Studies_Scrape.hayom_yom_scrape
+  #   puts hayom_yom.name
+  #   puts "            "
+  #   puts hayom_yom.text
+  #   puts "            "
+  #   puts "------------------------------"
+  #   puts hayom_yom.credits
+  #   puts "               "
+  #   puts "               "
+  #               #hayom_yoms.each do |output|
+  #               # puts "#{output.name}"
+  #               # puts "               "
+  #               # puts "#{output.text}"
+  #               # puts "-------------------------"
+  #               # puts " "
+  #               # puts "Credits:"
+  #               # puts "#{output.credits}"
+  #               # puts "                          "
+  #               # puts "                         "
+  #               # end
+  # end
+  
+  
   def self.hayom_yom
-    puts "Hayom Yom:"
-    hayom_yom = CLIPortfolioProject::Daily_Studies_Scrape.hayom_yom_scrape
-    puts hayom_yom.name
-    #hayom_yoms.each do |output|
-    # puts "#{output.name}"
-    # puts "               "
-    # puts "#{output.text}"
-    # puts "-------------------------"
-    # puts " "
-    # puts "Credits:"
-    # puts "#{output.credits}"
-    # puts "                          "
-    # puts "                         "
-    # end
+  hayom_yom = CLIPortfolioProject::Daily_Studies.find_by_text_study(@input).each do |output|
+  puts "#{output.name}"
+  puts "               "
+  puts "#{output.text}"
+  puts "-------------------------"
+  puts " "
+  puts "Credits:"
+  puts "#{output.credits}"
+  puts " "
+  puts "                         "
+  
+  end
   end
   
  def self.rambam
    puts "Daily Mitzvah:" 
-    rambams = CLIPortfolioProject::Daily_Studies_Scrape.rambam_array
-    rambams.each do |output|
-     puts "#{output.name}"
-     puts "                  "
-     puts "#{output.text}"
-     puts "-----------------------------"
-     puts " "
-     puts "Credits:"
-     puts "#{output.credits}"
-     puts "                              "
-     puts "                              "
+    rambam = CLIPortfolioProject::Daily_Studies_Scrape.rambam_scrape
+    puts rambam.name
+    puts "            "
+    puts rambam.text
+    puts "            "
+    puts "------------------------------"
+    puts rambam.credits
+    puts "               "
+    puts "               "
+    # rambams.each do |output|
+    # puts "#{output.name}"
+    # puts "                  "
+    # puts "#{output.text}"
+    # puts "-----------------------------"
+    # puts " "
+    # puts "Credits:"
+    # puts "#{output.credits}"
+    # puts "                              "
+    # puts "                              "
      puts "If you would like to see the unabridged version of the Mitzvah please type 'y'. Otherwise please type in anything. Thank you!"
      input = gets.strip
      if input == "y"
-       puts "#{output.full_text}"
+       puts rambam.full_text
        
      end
-    end
+    
   end
    
 end
--- lib/study.rb
+++ lib/study.rb
@@ -1,13 +1,12 @@
 class CLIPortfolioProject::Daily_Studies
    
    
-   attr_accessor  :hayom_yom, :rambam, :start, :scrape_all, :find_by_text_study
+   attr_accessor :name, :text, :credits, :full_text, :text_study, :hayom_yom, :rambam, :start, :scrape_all, :text_study, :find_by_text_study
     
       
    @@all = []
   
    def initialize()
-     
      @@all << self
    end
   
@@ -15,37 +14,55 @@ class CLIPortfolioProject::Daily_Studies
      @@all
    end
    
-   def self.find_by_text_study(text_study)
-     all.select do |daily_study|
+   def find_by_text_study(text_study)
+     @@all.select do |daily_study|
        daily_study.text_study == text_study
-     end
-   end
-   
-   def self.scrape_all
-     list_s = ['Hayom yom', 'Rambam']
-     
-     list_s.each do |s|
-      self.scrape_shiur(s)
-    
-   end
-   
-   
-   def self.scrape_shiur(shiur_name)
-    if shiur_name == 'Hayom yom' 
-      self.hayom_yom = CLIPortfolioProject::Daily_Studies_Scrape.hayom_yom_scrape
-      
-    elsif shiur_name == 'Rambam' 
-      self.rambam = CLIPortfolioProject::Daily_Studies_Scrape.rambam_scrape
+       end
     end
    
- end
+   
+   def self.rambam
+     rambam.text_study
+     rambam.name 
+     rambam.text
+     rambam.credits
+     rambam.full_text
+   end   
+   
+  def self.hayom_yom
+    hayom_yom.text_study
+    hayom_yom.name
+    hayom_yom.text
+    hayom_yom.credits
+  end
+   
+   
+   
+   
+   
+  # def self.scrape_all
+  #   list_s = ['Hayom yom', 'Rambam']
+     
+  #   list_s.each do |s|
+  #     self.scrape_shiur(s)
+  #   end
+  # end
+   
+   
+#   def self.scrape_shiur(shiur_name)
+#     if shiur_name == 'Hayom yom' 
+#       self.hayom_yom = CLIPortfolioProject::Daily_Studies_Scrape.hayom_yom_scrape
+      
+#     elsif shiur_name == 'Rambam' 
+#       self.rambam = CLIPortfolioProject::Daily_Studies_Scrape.rambam_scrape
+#     end
+   
+# end
  
  
 
   
 end
-end
-
 
 
 
