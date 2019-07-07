5a229bc5e054bbaa1b732a8a4102b27359981ec6
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-07 20:32:36 +0000

    delete commented out code

--- lib/study.rb
+++ lib/study.rb
@@ -27,37 +27,8 @@ class CLIPortfolioProject::Daily_Studies
        daily_study.text_study == text_study
        end
     end
-   
-   
-   
-   
-  # def self.scrape_all
-  #   list_s = ['Hayom yom', 'Rambam']
-     
-  #   list_s.each do |s|
-  #     self.scrape_shiur(s)
-  #   end
-  # end
-   
-   
-#   def self.scrape_shiur(shiur_name)
-#     if shiur_name == 'Hayom yom' 
-#       self.hayom_yom = CLIPortfolioProject::Daily_Studies_Scrape.hayom_yom_scrape
-      
-#     elsif shiur_name == 'Rambam' 
-#       self.rambam = CLIPortfolioProject::Daily_Studies_Scrape.rambam_scrape
-#     end
-   
-# end
- 
- 
-
   
 end
 
-
-
-# - add hayom_yom and rambam as attributes to a Daily Study object
-#  - add class methods that return just hayom_yom or rambam objects
 
 
