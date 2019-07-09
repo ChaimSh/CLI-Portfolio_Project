aa14943c9ce21bfcf45528c3aa9e2bd8c35a86e1
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-09 03:25:23 +0000

    extend extra rambam text

--- lib/cli.rb
+++ lib/cli.rb
@@ -26,38 +26,38 @@ class CLIPortfolioProject::CLI
   
   
   
-   def self.hayom_yom()
-     puts "Hayom Yom:"
-     CLIPortfolioProject::Daily_Studies.find_by_text_study("Hayom_yom").each do |output|
-        puts "#{output.name}"
-        puts "               "
-        puts "#{output.text}"
-        puts "-------------------------"
-        puts " "
-        puts "Credits:"
-        puts "#{output.credits}"
-        puts " "
-        puts "                         "
-      end
+  # def self.hayom_yom()
+  #   puts "Hayom Yom:"
+  #   CLIPortfolioProject::Daily_Studies.find_by_text_study("Hayom_yom").each do |output|
+  #       puts "#{output.name}"
+  #       puts "               "
+  #       puts "#{output.text}"
+  #       puts "-------------------------"
+  #       puts " "
+  #       puts "Credits:"
+  #       puts "#{output.credits}"
+  #       puts " "
+  #       puts "                         "
+  #     end
       
-    end
+    # end
   
-   def self.rambam()
-     puts "Daily Mitzvah:" 
+  # def self.rambam()
+  #   puts "Daily Mitzvah:" 
 
-     CLIPortfolioProject::Daily_Studies.find_by_text_study("Rambam").each do |output|
-        puts "#{output.name}"
-        puts "                  "
-        puts "#{output.text}"
-        puts "-----------------------------"
-        puts " "
-        puts "Credits:"
-        puts "#{output.credits}"
-        puts "                              "
-        puts "                              "
-       end
+  #   CLIPortfolioProject::Daily_Studies.find_by_text_study("Rambam").each do |output|
+  #       puts "#{output.name}"
+  #       puts "                  "
+  #       puts "#{output.text}"
+  #       puts "-----------------------------"
+  #       puts " "
+  #       puts "Credits:"
+  #       puts "#{output.credits}"
+  #       puts "                              "
+  #       puts "                              "
+  #     end
      
-    end
+  #   end
    
    def display_shiur(study_obj)
      study_obj.each do |output|
@@ -68,9 +68,25 @@ class CLIPortfolioProject::CLI
         puts " "
         puts "Credits:"
         puts "#{output.credits}"
+        puts "                               "
         puts "                              "
-        puts "                              "
-    # name = study_obj.name
+         if output.text_study == "Rambam"
+           extra_rambam_text(output)
+         end
+       end
+     end
+   
+   def extra_rambam_text(output)
+    puts "If you would like to see the unabridged version of the Mitzvah please type 'y'. Otherwise please type in anything. Thank you!"
+      input = gets.strip
+        if input == "y"
+         puts output.full_text
+         end
+       end
+   
+   
+   
+   # name = study_obj.name
     # text = study_obj.text
     # credits = study_obj.credits
     # puts name
@@ -83,19 +99,4 @@ class CLIPortfolioProject::CLI
     # puts "                              "
     # puts "                              "
     # puts shiur_text
-    end
-   end
-   
-   def extra_rambam_text(study_obj)
-    puts "If you would like to see the unabridged version of the Mitzvah please type 'y'. Otherwise please type in anything. Thank you!"
-      input = gets.strip
-        if input == "y"
-         puts study_obj.full_text
-         end
-       end
-   
-   
-   end
-   
-   
 end
