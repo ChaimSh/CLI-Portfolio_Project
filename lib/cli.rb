2bfa83ca48e849194852f2dffdf8164d687171f3
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-09 03:30:46 +0000

    clean up

--- lib/cli.rb
+++ lib/cli.rb
@@ -23,41 +23,7 @@ class CLIPortfolioProject::CLI
         end
       end
     end    
-  
-  
-  
-  # def self.hayom_yom()
-  #   puts "Hayom Yom:"
-  #   CLIPortfolioProject::Daily_Studies.find_by_text_study("Hayom_yom").each do |output|
-  #       puts "#{output.name}"
-  #       puts "               "
-  #       puts "#{output.text}"
-  #       puts "-------------------------"
-  #       puts " "
-  #       puts "Credits:"
-  #       puts "#{output.credits}"
-  #       puts " "
-  #       puts "                         "
-  #     end
-      
-    # end
-  
-  # def self.rambam()
-  #   puts "Daily Mitzvah:" 
 
-  #   CLIPortfolioProject::Daily_Studies.find_by_text_study("Rambam").each do |output|
-  #       puts "#{output.name}"
-  #       puts "                  "
-  #       puts "#{output.text}"
-  #       puts "-----------------------------"
-  #       puts " "
-  #       puts "Credits:"
-  #       puts "#{output.credits}"
-  #       puts "                              "
-  #       puts "                              "
-  #     end
-     
-  #   end
    
    def display_shiur(study_obj)
      study_obj.each do |output|
@@ -73,16 +39,16 @@ class CLIPortfolioProject::CLI
          if output.text_study == "Rambam"
            extra_rambam_text(output)
          end
-       end
-     end
+      end
+   end
    
    def extra_rambam_text(output)
     puts "If you would like to see the unabridged version of the Mitzvah please type 'y'. Otherwise please type in anything. Thank you!"
-      input = gets.strip
-        if input == "y"
-         puts output.full_text
-         end
-       end
+    input = gets.strip
+      if input == "y"
+        puts output.full_text
+      end
+   end
    
    
    
